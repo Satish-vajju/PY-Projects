88848594e5b0af89f36e5b4abaab63109c71067d
Satish-vajju <satishvajju@gmail.com>
2024-12-24 19:56:38 +0530

    up

---
+++ temp-conversion.py
@@ -0,0 +1,10 @@
+unit=input("select the temperature ('c' for Celsius or 'f' for Fahrenheit.(c/f): ").lower()
+temp=float(input("enter the temperature: "))
+if unit=="c":
+    temp=round((9*temp)/5+32,1)
+    print(f"temperature is {temp} °F")
+elif unit=="f":
+    temp=round((temp-32)*5/9,1)
+    print(f"temperature is {temp} °C")
+else:
+    print("Invalid unit. Please enter 'c' for Celsius or 'f' for Fahrenheit.")
